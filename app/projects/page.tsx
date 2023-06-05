dc3ff7e08ed2c535f1e8d7d3e4bc228e03b9e2db
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-05 19:22:22 +0200

    Laid out groundwork for Projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,17 @@
+
 'use client';
 
 import { motion } from 'framer-motion';
 import AnimatedList from '../components/animatedlist/animated-list';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const items = [
-  { title: 'Item 1', action: 'Action 1' },
-  { title: 'Item 2', action: 'Action 2' },
-  { title: 'Item 3', action: 'Action 3' },
+  { title: 'User management software', action: 'Action 1', image: '/City.jpg',content:'USER MANAGE',alt:'Picture of user management website' },
+  { title: 'Le mondays', action: 'Action 2', image: '/frog.jpg',content:'LE MONDAYS',alt:'Picture of website le-mondays' },
+  { title: 'Carelyo Dashboard', action: 'Action 3', image: '/bird.jpg',content:'CARELYO',alt:'picture of carelyo dashboard' },
+  { title: 'E-commerce school project', action: 'Action 4',content:'E-COMMERCE',alt:'picture of e-commerce school project' },
+  { title: 'Untitled jewelry website ', action: 'Action 5',content:'Jewelry Page',alt:'picture of untitled jewelry page' },
 ];
 
 const container = {
@@ -42,6 +46,10 @@ const image ='/City.jpg'
 const images = new Array(10).fill(null);
 
 const Projects = () => {
+    const[projectChoice, setProjectChoice] = useState(items[0].action);
+    const selectedItem = items.find(item => item.action === projectChoice);
+
+    
   return (
     <div className='flex h-[90vh] w-full px-4 pt-4 '>
       <div className='grow basis-1/4 '>
@@ -50,9 +58,9 @@ const Projects = () => {
             <h1 className=' font-extrabold text-4xl absolute z-10 pt-4'>My Projects</h1>
           </div>
           <div className='grow  relative overflow-hidden'>
-            <div className='flex flex-col h-full w-full relative top-48'>
+            <div className='flex flex-col h-full w-full relative '>
               <div className=' basis-1/6 p-2 z-10 '>
-                <AnimatedList items={items} />
+                <AnimatedList items={items} setProjectChoice={setProjectChoice} />
               </div>
             </div>
             <motion.div
@@ -78,11 +86,11 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <div className=' basis-3/4 border  flex flex-col '>
-        <div className='justify-center flex basis-1/12 border'>Titel</div> 
-        <div className='justify-center  flex  basis-2/12 border'>Written content Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque adipisci, accusantium suscipit minima sapiente dolorum error obcaecati placeat? Rem, ex saepe repudiandae eligendi suscipit itaque assumenda officiis praesentium quod quis?</div>
-        <div className='justify-center  flex basis-9/12 border relative '>  <Image  fill src={'/city.jpg'} alt='test' /> </div>
-
+      <div className=' basis-3/4 border  flex flex-col border-lime-500 '>
+        <div className='justify-center flex basis-1/12  text-4xl text-lime-500'>{selectedItem?.title}</div> 
+        <div className='justify-center  flex  basis-2/12 border-t border-lime-500'>{selectedItem?.content}Written content Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque adipisci, accusantium suscipit minima sapiente dolorum error obcaecati placeat? Rem, ex saepe repudiandae eligendi suscipit itaque assumenda officiis praesentium quod quis?</div>
+        <div className='justify-center  flex basis-9/12 border-t relative border-lime-500'> {selectedItem?.image && <Image  fill src={selectedItem.image} alt={selectedItem?.alt} />} </div>
+                    
       </div>
     </div>
   );
