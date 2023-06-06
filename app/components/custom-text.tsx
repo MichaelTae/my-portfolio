73b080235990a9dee146adfb3782ff6453f1a8cb
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-06 21:56:35 +0200

    fixed error if navigated before full mounted comp

--- app/components/custom-text.tsx
+++ app/components/custom-text.tsx
@@ -30,33 +30,43 @@ export const TitleText = ({ title, textStyles }:{title:string, textStyles:string
 
 
 export const AnimatedTextSorting = ({ delay = 0 }) => {
-    const controls = useAnimation();
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const targetValue = "I've kinda ran out of things to say about myself but in the interest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me!";
-    const intervalRef = useRef<NodeJS.Timeout | undefined>();
-    const [headerText, setHeaderText] = useState("I've kinda ran out of things to say about myself but in the interest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me!");
-  
-    useEffect(() => {
-      setTimeout(() => {
-        let iteration = 0;
-        intervalRef.current = setInterval(() => {
-          setHeaderText(targetValue.slice(0, Math.ceil(iteration))
-            + targetValue.slice(Math.ceil(iteration))
-              .replace(/./g, () => letters[Math.floor(Math.random() * 26)])
-          );
-          if (iteration >= targetValue.length) {
-            if (intervalRef.current) clearInterval(intervalRef.current);
-          }
-          iteration += 2 / 3;
-        }, 30);
-        controls.start({opacity: 1});  
-      }, delay);
-  
-      return () => {
-        if (intervalRef.current) clearInterval(intervalRef.current);
-      };
-    }, [delay]);
-  
-    return <motion.p animate={controls} initial={{opacity: 0}} >{headerText}</motion.p>;
-  };
+  const controls = useAnimation();
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const targetValue = "I've kinda ran out of things to say about myself but in the interest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me!";
+  const isMounted = useRef(true); 
+  const [headerText, setHeaderText] = useState(targetValue);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    const startAnimation = () => {
+      let iteration = 0;
+      intervalId = setInterval(() => {
+        setHeaderText(targetValue.slice(0, Math.ceil(iteration))
+          + targetValue.slice(Math.ceil(iteration))
+            .replace(/./g, () => letters[Math.floor(Math.random() * 26)])
+        );
+        if (iteration >= targetValue.length) {
+          clearInterval(intervalId);
+        }
+        iteration += 2 / 3;
+      }, 30);
+
+      if (isMounted.current) {
+        controls.start({opacity: 1}).catch(()=>{});
+      }
+    };
+
+    const timeoutId = setTimeout(startAnimation, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+      clearInterval(intervalId);
+      isMounted.current = false;
+    };
+  }, [delay]);
+
+  return <motion.p animate={controls} initial={{opacity: 0}} >{headerText}</motion.p>;
+};
+
   
