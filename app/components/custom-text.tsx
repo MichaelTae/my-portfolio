e575deaae21ea614b7f6c148e0b3706a286e4ee6
Michael S <Michael.Stojanovic@hotmail.com>
2023-06-08 16:47:41 +0200

    try fix for navigation bottleneck

--- app/components/custom-text.tsx
+++ app/components/custom-text.tsx
@@ -33,40 +33,37 @@ export const AnimatedTextSorting = ({ delay = 0 }) => {
   const controls = useAnimation();
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const targetValue = "I've kinda ran out of things to say about myself but in the interest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me!";
-  const isMounted = useRef(true); 
   const [headerText, setHeaderText] = useState(targetValue);
 
   useEffect(() => {
-    let intervalId: NodeJS.Timeout;
-
-    const startAnimation = () => {
-      let iteration = 0;
-      intervalId = setInterval(() => {
-        setHeaderText(targetValue.slice(0, Math.ceil(iteration))
-          + targetValue.slice(Math.ceil(iteration))
-            .replace(/./g, () => letters[Math.floor(Math.random() * 26)])
-        );
-        if (iteration >= targetValue.length) {
-          clearInterval(intervalId);
-        }
-        iteration += 2 / 3;
-      }, 30);
-
-      if (isMounted.current) {
-        controls.start({opacity: 1}).catch(()=>{});
+    let animationFrameId = 0;
+    let iteration = 0;
+    
+    const animateText = () => {
+      setHeaderText(
+        targetValue.slice(0, Math.ceil(iteration)) +
+          targetValue.slice(Math.ceil(iteration)).replace(/./g, () => letters[Math.floor(Math.random() * 26)])
+      );
+      if (iteration < targetValue.length) {
+        iteration += 1 / 3;
+        animationFrameId = requestAnimationFrame(animateText);
       }
     };
-
-    const timeoutId = setTimeout(startAnimation, delay);
-
+    
+    const timeoutId = setTimeout(() => {
+      animationFrameId = requestAnimationFrame(animateText);
+      controls.start({ opacity: 1 }).catch(() => {});
+    }, delay);
+    
     return () => {
       clearTimeout(timeoutId);
-      clearInterval(intervalId);
-      isMounted.current = false;
+      cancelAnimationFrame(animationFrameId);
+      controls.stop();
     };
-  }, [delay,controls]);
+  }, [delay, controls]);
 
-  return <motion.p animate={controls} initial={{opacity: 0}} >{headerText}</motion.p>;
+  return <motion.p animate={controls} initial={{ opacity: 0 }}>{headerText}</motion.p>;
 };
+
 
   
