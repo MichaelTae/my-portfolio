593561a960f497cf7d874121caf5a5b9bc4023aa
Michael S <Michael.Stojanovic@hotmail.com>
2023-07-06 18:59:56 +0200

    added wrapper + css

---
+++ app/components/background/backgroundWrapper.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useState } from "react";
+import { BackSvg } from "./BackSvg";
+import RainAnimation from "../rain_background/rainAnimation";
+import CustomAudioPlayer from "../custom_audio_player/customAudioPlayer";
+
+
+const BackgroundWrapper = () => {
+    const [highlighted, setHighlighted] = useState('');
+    
+    return (
+        <div className=' z-0 relative h-[100vh] overflow-hidden '>
+             <RainAnimation/>
+          <BackSvg highlighted={highlighted} setHighlighted={setHighlighted}/>
+          <CustomAudioPlayer/>
+        </div>
+    )
+
+};
+
+
+export default BackgroundWrapper;
