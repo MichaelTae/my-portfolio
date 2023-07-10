5fa7f8df52a6290651936fe35a8d1322f445bc92
Michael S <Michael.Stojanovic@hotmail.com>
2023-07-10 12:00:33 +0200

    list css change

--- app/components/animatedlist/animated-list.tsx
+++ app/components/animatedlist/animated-list.tsx
@@ -16,7 +16,7 @@ const variantsMobile = {
 
 const AnimatedListItem = ({ title, action,setProjectChoice  }:{title:string,action:string,setProjectChoice:Function}) => (
     <motion.li
-        className={"py-1 px-1 sm:py-2 lg:px-4 text-lime-500 font-mono font-semibold cursor-pointer  text-sm sm:text-md lg:text-lg text-center border border-lime-500 rounded-md backdrop-filter backdrop-blur-sm  hover:bg-opacity-40 hover:bg-lime-500 hover:text-white transition-all "}
+        className={"py-1 px-1 sm:py-2 lg:px-4 text-lime-500 font-orbitron font-semibold cursor-pointer  text-sm sm:text-md lg:text-lg text-center border border-lime-500 rounded-md backdrop-filter backdrop-blur-sm  hover:bg-opacity-40 hover:bg-lime-500 hover:text-white transition-all hover:scale-105 active:scale-100 "}
         onClick={() => setProjectChoice(action)}
     >
         {title}
@@ -26,7 +26,7 @@ const AnimatedListItem = ({ title, action,setProjectChoice  }:{title:string,acti
 const AnimatedList = ({ items,setProjectChoice  }:{items:any, setProjectChoice: Function}) => {
     const isNotMobile = useMediaQuery('(min-width: 640px)');
 
-    return(<motion.ul className="space-y-2 py-1"  variants={isNotMobile ? variants : variantsMobile}
+    return(<motion.ul className="space-y-2 py-1 px-2"  variants={isNotMobile ? variants : variantsMobile}
         initial="hidden"
         animate="visible"
         exit="hidden">
