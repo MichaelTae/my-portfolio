6a45cf0c5bc7ebc6f5ce2905dd6b0608db7457b5
Michael S <Michael.Stojanovic@hotmail.com>
2023-07-21 18:49:59 +0200

    accessability added to list nav

--- app/components/animatedlist/animated-list.tsx
+++ app/components/animatedlist/animated-list.tsx
@@ -30,6 +30,14 @@ const AnimatedListItem = ({
       'py-1 px-1 sm:py-2 lg:px-4 text-lime-500 font-orbitron font-semibold cursor-pointer  text-sm sm:text-md lg:text-lg text-center border border-lime-500 rounded-md backdrop-filter backdrop-blur-sm backdrop-brightness-50 -backdrop-hue-rotate-90  hover:bg-opacity-40 hover:bg-lime-500 hover:text-white transition-all hover:scale-105 active:scale-100 '
     }
     onClick={() => setProjectChoice(action)}
+    tabIndex={0}
+    role='button'
+    onKeyDown={(e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        setProjectChoice(action);
+      }
+    }}
+    aria-label={`view ${title} project`}
   >
     {title}
   </motion.li>
