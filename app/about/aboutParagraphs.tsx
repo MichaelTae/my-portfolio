37a50d204f6c77a176b8c36c798622c49f00a652
Michael S <Michael.Stojanovic@hotmail.com>
2023-09-25 19:02:44 +0200

    Grammar fixes

--- app/about/aboutParagraphs.tsx
+++ app/about/aboutParagraphs.tsx
@@ -3,13 +3,13 @@ export const introParagraph =
 export const introParagraph2 =
   'I am currently looking for a job as a junior developer. My education made me full stack so I am comfortable with both frontend and backend. I am also open to learning new languages and frameworks.';
 export const symmetryParagraph =
-  "I've kinda ran out of things to say about myself but in theinterest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me on the next page --->";
+  "I've kinda ran out of things to say about myself but in the interest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me on the next page --->";
 export const languageParagraph1 =
   " The languages that I'm most comfortable with are C# and Javascript.";
 export const languageParagraph2 =
-  " I'm also comfortable with React, Next.js, Tailwind, HTML,CSS, SQL, Node.js and .NET. and lastly familiar with Git,Azure DevOps, Azure, Illustrator";
+  " I'm also comfortable with React, Next.js, Tailwind, HTML,CSS, SQL, Node.js, .NET. and lastly familiar with Git, Azure Dev/portal and Illustrator";
 export const siteParagraph1 =
-  'This page is built with React, Next.js , Tailwind and the interactable parts of the background on the homepage is made with illustrator,';
+  'This page is built with React, Next.js, Tailwind and the interactable parts of the background on the homepage is made with illustrator,';
 export const siteParagraph2 =
   "I'm using framer motion also for fun...? animations";
 export const siteParagraph3 = ' (i do like animations quite a lot).';
