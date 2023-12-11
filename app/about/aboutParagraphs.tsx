8c8711e8b18cd9f7b89c46d86b44a3ec7b975d12
Michael S <Michael.Stojanovic@hotmail.com>
2023-12-11 14:25:01 +0100

    changed text about section

--- app/about/aboutParagraphs.tsx
+++ app/about/aboutParagraphs.tsx
@@ -1,17 +1,23 @@
 export const introParagraph =
-  'My name is Michael Stojanovic, I am a recent graduate from Nackademin. There I studied Web development for 2 years with a focus on .NET CMS. I very much enjoy programming and getting better at it.';
+  'My name is Michael Stojanovic, a recent graduate from Nackademin, specializing in Web Development with a two-year focus on .NET CMS. My passion lies in programming, and I am dedicated to continuous improvement in this field.';
+
 export const introParagraph2 =
-  'I am currently looking for a job as a junior developer. My education made me full stack so I am comfortable with both frontend and backend. I am also open to learning new languages and frameworks.';
+  'I am currently in pursuit of opportunities as a junior developer, my comprehensive education has equipped me with full-stack proficiency, ensuring comfort and competence in both front-end and back-end development.';
+
 export const symmetryParagraph =
-  "I've kinda ran out of things to say about myself but in the interest of symmetry I'll just write a few more lines. Thank you for reading and if you have any questions feel free to contact me on the next page --->";
+  'Thank you for taking the time and reading my About section! Should you want to know more about me or my work, my contact details are on the following page. I appreciate any feedback and or questions you might have for me. --->';
+
 export const languageParagraph1 =
-  " The languages that I'm most comfortable with are C# and Javascript.";
+  'My primary languages of expertise are C# and Javascript(React).';
+
 export const languageParagraph2 =
-  " I'm also comfortable with React, Next.js, Tailwind, HTML,CSS, SQL, Node.js, .NET. and lastly familiar with Git, Azure Dev/portal and Illustrator";
+  'In addition, I am adept with technologies such as React, Next.js, Tailwind, HTML, CSS, SQL, Node.js, .NET, and possess familiarity with Git, Azure Dev/portal, and Illustrator.';
+
 export const siteParagraph1 =
-  'This page is built with React, Next.js, Tailwind and the interactable parts of the background on the homepage is made with illustrator,';
+  'This portfolio is crafted using React, Next.js, and Tailwind. The interactive elements on the homepage are SVGs designed with Illustrator.';
+
 export const siteParagraph2 =
-  "I'm using framer motion also for fun...? animations";
-export const siteParagraph3 = ' (i do like animations quite a lot).';
+  'Using framer motion for animations, making my appreciation for motion design apparent.';
+
 export const deployedParagraph1 =
-  'I have a few projects on my github that you can check out. I have so far deployed two websites, which are:';
+  'Visit my portfolio further on GitHub, where I have a collection of projects. Notably, I have  deployed two websites:';
