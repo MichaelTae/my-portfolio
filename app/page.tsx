87b027dc29b0f9a4ab2515f35d4ad45e5c53f75c
Michael S <Michael.Stojanovic@hotmail.com>
2023-12-11 14:26:07 +0100

    fixed welcome message layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
     <motion.main className='flex-1  relative justify-center items-center  bg-transparent  '>
       <div className='flex z-10 absolute flex-col  sm:w-[100%] md:w-[100%] lg:w-[35%] right-0 p-4   pointer-events-none'>
         <div className='flex order-2 flex-col  gap-1 '>
-          <motion.div className='flex justify-end'>
+          <motion.div className='flex justify-end  '>
             <TitleText
               title=' Michael Stojanovic'
-              textStyles=' pl-2 flex  text-slate-300 backdrop-blur-sm sm:backdrop-blur-0 backdrop-brightness-50 font-orbitron shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] drop-shadow-2xl border border-green-400/40 rounded-xl p-1  '
+              textStyles=' pl-2 flex text-slate-300 backdrop-blur-sm  w-full sm:backdrop-blur-0 backdrop-brightness-50 font-orbitron shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] drop-shadow-2xl border border-green-400/40 rounded-xl p-1  '
             />
           </motion.div>
           <motion.div
@@ -36,7 +36,7 @@ export default function Home() {
             initial='hidden'
             whileInView={'show'}
             variants={fadeIn('bottom', 'ease', 1, 1)}
-            className=' p-1 xl:mt-52 2xl:mt-0 pl-8 backdrop-brightness-50 backdrop-blur-sm sm:backdrop-blur-0 text-slate-200 shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] drop-shadow-2xl border border-green-400/40 rounded-xl'
+            className=' p-1 xl:mt-52 2xl:mt-0 pl-8 backdrop-brightness-50 backdrop-blur-sm sm:backdrop-blur-0 font-orbitron text-md text-slate-300 shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] drop-shadow-2xl border border-green-400/40 rounded-xl'
           >
             Hello! Welcome to my portfolio page, here I show some of the stuff I
             have been working on during my education and internship. If
@@ -48,7 +48,7 @@ export default function Home() {
           initial='hidden'
           whileInView={'show'}
           variants={fadeIn('top', 'ease', 0.5, 1)}
-          className=' self-center min-[600px]:self-end lg:self-center '
+          className=' self-center'
         >
           <Image
             src='/123me.jpg'
