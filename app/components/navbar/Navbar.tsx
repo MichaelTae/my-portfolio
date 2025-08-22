bacf66000b61dc7f715156837f2957b0cc366154
Michael S <Michael.Stojanovic@hotmail.com>
2025-08-22 14:44:17 +0200

    added nav to homepage to avoid confusing navigation

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -35,11 +35,7 @@ const Navbar = () => {
   const path = usePathname();
 
   return (
-    <nav
-      className={`${
-        path === '/' ? 'lg:hidden ' : 'relative scale-100'
-      } bg-slate-900/50 w-full flex flex-col  z-30    `}
-    >
+    <nav className={` bg-slate-900/50 w-full flex flex-col  z-30    `}>
       <div className='animate-pulse pointer-events-none h-2 relative order-4 z-0 shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)] drop-shadow-2xl'></div>
       <div className=' flex sticky justify-between lg:justify-normal '>
         <div className='flex  gap-4 p-4 '>
